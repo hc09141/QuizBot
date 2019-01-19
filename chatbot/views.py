bfd2f70e9b95780bb93ac2e6c061b43f10aaf661
hc09141 <hc09141@users.noreply.github.com>
2019-01-19 17:30:04 +0000

    Echoes Facebook messages

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,5 +1,30 @@
+import requests
+
 from django.http import HttpResponse
 
-
-def index(request):
+# Allows FB to validate our app
+def get(request):
     return HttpResponse(request.GET['hub.challenge'])
+
+# For receiving user messages
+def post(self, request, *args, **kwargs):
+    # Converts the text payload into a python dictionary
+    incoming_message = json.loads(self.request.body.decode('utf-8'))
+    # Facebook recommends going through every entry since they might send
+    # multiple messages in a single call during high load
+    for entry in incoming_message['entry']:
+        for message in entry['messaging']:
+            # Check to make sure the received call is a message call
+            # This might be delivery, optin, postback for other events 
+            if 'message' in message:
+                process_message(entry['messaging']['sender']['id'])
+    return HttpResponse()
+
+ def process_message(fb_id, msg):
+	 # greeting = "Hi John! I'm alive!"
+	 post_facebook_message(fb_id, msg)
+
+def post_facebook_message(fbid, message):         
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' %(FACEBOOK_ACCESS_TOKEN) 
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
