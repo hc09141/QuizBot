300bd33635e39abe9f52f23dbb0e3f588e7d01a2
hc09141 <hc09141@users.noreply.github.com>
2019-01-19 21:19:58 +0000

    Make DB work on Heroku pls

--- quizbot/settings.py
+++ quizbot/settings.py
@@ -77,12 +77,17 @@ WSGI_APPLICATION = 'quizbot.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+# ie if Heroku server
+# if 'DATABASE_URL' in os.environ:
+import dj_database_url
+DATABASES = {'default': dj_database_url.config()}
 
 
 # Password validation
@@ -127,8 +132,3 @@ STATICFILES_DIRS = (
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-# ie if Heroku server
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
