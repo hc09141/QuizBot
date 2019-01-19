71210c237580029249294ac1df550b15c98992d0
hc09141 <hc09141@users.noreply.github.com>
2019-01-19 16:58:16 +0000

    Adds webhook for facebook

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Chatbot index.")
+    return HttpResponse(request.GET['hub.challenge'])
