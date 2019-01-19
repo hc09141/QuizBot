23d67436fc84e9d7e3dcc1c498d18973f25a8636
hc09141 <hc09141@users.noreply.github.com>
2019-01-19 18:54:34 +0000

    Finally echoes FB messenger?

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 from django.http import HttpResponse
@@ -5,39 +6,36 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def index(request):
-    print(request.method)
-    print(request.body)
-    if request.GET:
-        return HttpResponse(request.GET['hub.challenge'])
-    else:
-        return HttpResponse()
+    if request.method == 'GET':
+        return validate(request)
+    return process_messages(request)
 
-# # Allows FB to validate our app
-# def validate(request):
-#     print('Get')
-#     return HttpResponse(request.GET['hub.challenge'])
+# Allows FB to validate our app
+def validate(request):
+    print('Get')
+    return HttpResponse(request.GET['hub.challenge'])
 
-# # For receiving user messages
-# def post(self, request, *args, **kwargs):
-#     print('Post')
-#     # Converts the text payload into a python dictionary
-#     incoming_message = json.loads(self.request.body.decode('utf-8'))
-#     # Facebook recommends going through every entry since they might send
-#     # multiple messages in a single call during high load
-#     for entry in incoming_message['entry']:
-#         for message in entry['messaging']:
-#             # Check to make sure the received call is a message call
-#             # This might be delivery, optin, postback for other events 
-#             if 'message' in message:
-#                 print(message['message']['text'])
-#                 process_message(entry['messaging']['sender']['id'], message['message']['text'])
-#     return HttpResponse()
+# For receiving user messages
+def process_messages(self, request, *args, **kwargs):
+    print('Processing messages')
+    # Converts the text payload into a python dictionary
+    incoming_message = json.loads(self.request.body.decode('utf-8'))
+    # Facebook recommends going through every entry since they might send
+    # multiple messages in a single call during high load
+    for entry in incoming_message['entry']:
+        for message in entry['messaging']:
+            # Check to make sure the received call is a message call
+            # This might be delivery, optin, postback for other events 
+            if 'message' in message:
+                print(message['message']['text'])
+                process_message(entry['messaging']['sender']['id'], message['message']['text'])
+    return HttpResponse()
 
-# def process_message(fb_id, msg):
-#     # greeting = "Hi John! I'm alive!"
-#     post_facebook_message(fb_id, msg)
+def process_message(fb_id, msg):
+    # greeting = "Hi John! I'm alive!"
+    post_facebook_message(fb_id, msg)
 
-# def post_facebook_message(fbid, message):         
-#     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' %(FACEBOOK_ACCESS_TOKEN) 
-#     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message}})
-#     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+def post_facebook_message(fbid, message):         
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' %os.environ["PAGE_ACCESS_TOKEN"] 
+    response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":message}})
+    status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
