436d23f76163cf14327152d4a5fe1e63aaee0d2e
hc09141 <hc09141@users.noreply.github.com>
2019-01-20 01:16:59 +0000

    admin stuff

--- chatbot/admin.py
+++ chatbot/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+class MessageAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(Message, MessageAdmin)
